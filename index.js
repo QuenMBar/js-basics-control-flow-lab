60ee86f3ec9ee28935b4fc0339bbb21a2499a8d5
QuenMBar <Qbeast360@gmail.com>
2021-02-22 11:53:26 -0800

    commit tree 0d3c55561340b8759b2c6cd365e2ec54a1ecb5d7

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+    // Works, but is kinda hacky I feel
+    // switch (true) {
+    //     case distance <= 400:
+    //         return 'This one is on me!';
+    //         break;
+
+    //     case distance <= 2000:
+    //         // Nothing to test for case
+    //         break;
+
+    //     case distance <= 2500:
+    //         return 'I will gladly take your thirty bucks.';
+    //         break;
+
+    //     default:
+    //         return 'No can do.';
+    //         break;
+    // }
+    if (distance <= 400) {
+        return 'This one is on me!';
+    } else if (distance > 2500) {
+        return 'No can do.';
+    } else {
+        return 'I will gladly take your thirty bucks.';
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+    if (city === 'NYC') {
+        return "Ok, sounds good.";
+    } else {
+        return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+    switch (tip) {
+        case 'generous':
+            return 'Thank you so much.';
+            break;
+        case 'not as generous':
+            return 'Thank you.';
+            break;
+        case 'thanks for everything':
+            return 'Bye.';
+            break;
+}
 }
